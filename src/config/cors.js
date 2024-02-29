c7c48c0d93ea7263509bae2e10f708e239354849
phuongNV <nvp.1312.2001@gmail.com>
2024-03-01 01:26:16 +0700

    fix bugs 4

--- src/config/cors.js
+++ src/config/cors.js
@@ -4,18 +4,18 @@ const configCors = (app) => {
   // Add headers before the routes are defined
   app.use(function (req, res, next) {
     // Website you wish to allow to connect
-    res.setHeader("Access-Control-Allow-Origin", process.env.REACT_URL);
+    res.setHeader("Access-Control-Allow-Origin", "*");
 
     // Request methods you wish to allow
     res.setHeader(
       "Access-Control-Allow-Methods",
-      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+      "GET, HEAD, POST, OPTIONS, PUT, PATCH, DELETE"
     );
 
     // Request headers you wish to allow
     res.setHeader(
       "Access-Control-Allow-Headers",
-      "X-Requested-With,content-type,Authorization"
+      "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
     );
 
     // Set to true if you need the website to include cookies in the requests sent
